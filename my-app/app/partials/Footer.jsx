35ee9f685a6759c6a02a9ee768041eadb97d5d1d
Hassan Zahid <hassan@visnext.net>
2026-03-17 21:27:42 +0700

    footer is fixed

--- my-app/app/partials/Footer.jsx
+++ my-app/app/partials/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="border-t border-stone-200 bg-white py-6">
       <div className=" text-center sm:text-left max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 ">
        
-        <p className="text-xs text-stone-400 text-center">© {new Date().getFullYear()} Inkwell. All rights reserved.</p>
+        <p className="text-xs text-stone-400 text-center">© {new Date().getFullYear()} All rights reserved.</p>
       </div>
     </footer>
   );
